59f6622c37b5b4d06907154c07e7b2fcc5c2a070
liqian024 <liqian024@qq.com>
2019-11-12 11:18:22 +0800

    no message

--- src/main/java/com/fh/controller/exam/testpaper/TestPaperController.java
+++ src/main/java/com/fh/controller/exam/testpaper/TestPaperController.java
@@ -4,6 +4,7 @@ import java.io.PrintWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -16,6 +17,8 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.alibaba.fastjson.JSON;
 import com.fh.controller.base.BaseController;
 import com.fh.entity.Page;
 import com.fh.entity.system.User;
@@ -26,6 +29,11 @@ import com.fh.util.PageData;
 import com.fh.util.Jurisdiction;
 import com.fh.util.Tools;
 import com.fh.util.date.DateUtils;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import com.fh.service.exam.testmain.TestMainManager;
 import com.fh.service.exam.testpaper.TestPaperManager;
 
 /** 
@@ -41,6 +49,8 @@ public class TestPaperController extends BaseController {
 	@Resource(name="testpaperService")
 	private TestPaperManager testpaperService;
 	
+	@Resource(name="testmainService")
+	private TestMainManager testmainService;
 	/**保存
 	 * @param
 	 * @throws Exception
@@ -240,21 +250,131 @@ public class TestPaperController extends BaseController {
 		ModelAndView mv = this.getModelAndView();
 		PageData pd = new PageData();
 		pd = this.getPageData();
-		pd.put("TEST_PAPER_ID", 1);
-		page.setPd(pd);
-		List<PageData>	varList = testpaperService.listExam(page);	//列出TestPaper列表
-		double score= Double.parseDouble(varList.get(0).get("TEST_PAPER_SCORE").toString());
-		int num=Integer.parseInt(varList.get(0).get("TEST_QUESTION_NUM").toString());
+		pd.put("TEST_PAPER_ID","EP20191107144218001");
 		User user = (User) Jurisdiction.getSession().getAttribute(Const.SESSION_USERROL);
 	    String userId=user.getUSER_ID();
+		pd.put("TEST_USER", userId);
+		pd.put("TEST_PLAN_ID", "EP20191111113555283");//考试计划
+		//根据考试计划、试卷ID和考试人判断该用户是否已参加过考试
+		List<PageData> userExamList=testmainService.listByUser(pd);
+		if(null!=userExamList&&userExamList.size()>0){
+			mv.setViewName("exam/testonline/Warning");
+			mv.addObject("pd", pd);
+			return mv;
+		}
+		page.setPd(pd);
+		List<PageData>	varList = testpaperService.listExam(page);	//列出TestPaper列表
+		List<PageData>	answerList = testpaperService.listAnswer(page);	//列出TestPaper列表
+		double score= Double.parseDouble(varList.get(0).get("TEST_PAPER_SCORE").toString());
+//		User user = (User) Jurisdiction.getSession().getAttribute(Const.SESSION_USERROL);
+//	    String userId=user.getUSER_ID();
 		mv.setViewName("exam/testonline/examOnline");
 		mv.addObject("varList", varList);
+		mv.addObject("answerList", answerList);
 		pd.put("TEST_PAPER_SCORE", score);
-		pd.put("TEST_QUESTION_NUM", num);
+		pd.put("TEST_QUESTION_NUM", varList.size());
 		pd.put("TEST_USER", userId);
 		pd.put("EXAM_TIME", DateUtils.getCurrentTime());
 		pd.put("ANSWER_TIME", Integer.parseInt(varList.get(0).get("ANSWER_TIME").toString()));
 		mv.addObject("pd", pd);
 		return mv;
+	}	
+	/**计算考试结果
+	 * @param
+	 * @throws Exception
+	 */
+	@RequestMapping(value="/examResult")
+	public @ResponseBody PageData examResult(Page page)throws Exception{	
+		PageData pd = new PageData();
+		pd = this.getPageData();
+		User user = (User) Jurisdiction.getSession().getAttribute(Const.SESSION_USERROL);
+	    String userId=user.getUSER_ID();
+		pd.put("TEST_USER", userId);
+		pd.put("TEST_PLAN_ID", "EP20191111113555283");//考试计划
+		//根据考试计划、试卷ID和考试人判断该用户是否已参加过考试
+		List<PageData> userExamList=testmainService.listByUser(pd);
+		if(null!=userExamList&&userExamList.size()>0){
+			pd.put("msg", "该用户已参加过考试，不允许再考！");
+			return pd;
+		}
+		List<PageData> answerList=new ArrayList<PageData>();//答案集合		 
+		JSONObject jsonobject = JSONObject.fromObject(pd.getString("result"));
+		//考试分数
+		int score=0;
+		page.setPd(pd);
+		//获取该卷纸试题相关信息,包括答案得分等
+		List<PageData>	varList = testpaperService.listExam(page);	//列出TestPaper列表
+		for(PageData p:varList){	
+			//单选
+			if(p.getString("TEST_QUESTION_TYPE").equals("1")){
+				PageData pdAnswer = new PageData();//答案
+				String rightAnswer=p.getString("TEST_QUESTION_ANSWER");
+				//判断该单选题题是否作答
+				if (jsonobject.has(p.getString("TEST_QUESTION_ID"))){
+					String answer=jsonobject.getString(p.getString("TEST_QUESTION_ID"));	
+					pdAnswer.put("TEST_QUESTION_ID", p.getString("TEST_QUESTION_ID"));
+					pdAnswer.put("TEST_ANSWER", answer);
+					pdAnswer.put("TEST_CORRECT_ANSWER", rightAnswer);
+					if(answer.equals(rightAnswer)){
+						score=score+Integer.parseInt(p.get("TEST_QUESTION_SCORE").toString());
+						}
+				}else{
+					//未作答，答案为空
+					pdAnswer.put("TEST_QUESTION_ID", p.getString("TEST_QUESTION_ID"));
+					pdAnswer.put("TEST_ANSWER", "");
+					pdAnswer.put("TEST_CORRECT_ANSWER", rightAnswer);
+				}
+				answerList.add(pdAnswer);
+			}else if(p.getString("TEST_QUESTION_TYPE").equals("2")){
+				String rightAnswer=p.getString("TEST_QUESTION_ANSWER");
+				PageData pdAnswer = new PageData();//答案
+				//判断该多选题题是否作答
+				if (jsonobject.has(p.getString("TEST_QUESTION_ID"))){
+					String answer=jsonobject.getString(p.getString("TEST_QUESTION_ID"));
+			        if (answer.indexOf("#&#")!=-1){
+			        	answer=answer.replaceAll("#&#", ",");
+			        }
+					pdAnswer.put("TEST_QUESTION_ID", p.getString("TEST_QUESTION_ID"));
+					pdAnswer.put("TEST_ANSWER", answer);
+					pdAnswer.put("TEST_CORRECT_ANSWER", rightAnswer);
+					//将多选题答案进行排序并比较
+					byte[] b1 = answer.getBytes();
+					byte[] b2 = rightAnswer.getBytes();
+					Arrays.sort(b1);
+					Arrays.sort(b2);
+					answer = new String(b1);
+					rightAnswer = new String(b2);
+					if (answer.equals(rightAnswer)) {
+						score=score+Integer.parseInt(p.get("TEST_QUESTION_SCORE").toString());
+					}
+				}else{
+					pdAnswer.put("TEST_QUESTION_ID", p.getString("TEST_QUESTION_ID"));
+					pdAnswer.put("TEST_ANSWER", "");
+					pdAnswer.put("TEST_CORRECT_ANSWER", rightAnswer);
+				}
+				answerList.add(pdAnswer);
+			}			
+		}
+		//纸卷分数
+		double paperScore= Double.parseDouble(varList.get(0).get("TEST_PAPER_SCORE").toString());
+		//合格分数
+		double qualifiedScore= Double.parseDouble(varList.get(0).get("QUALIFIED_SCORE").toString());
+		pd.put("TEST_TIME",DateUtils.getCurrentTime() );//考试时间
+		pd.put("TEST_PAPER_SCORE", paperScore);
+		pd.put("QUALIFIED_SCORE", qualifiedScore);
+		pd.put("TEST_SCORE", score);//考试分分数
+		if(score>=qualifiedScore){
+			pd.put("IF_QUALIFIED", '1');//是否合格
+		}else{
+			pd.put("IF_QUALIFIED", '0');//是否合格
+		}
+		pd.put("answerList", answerList);
+		pd.put("TEST_COUNT", 1);
+		pd.put("CREATE_USER", userId);//创建人
+		pd.put("TEST_QUESTION_NUM", varList.size());//题目数
+		pd.put("CREATE_DATE", DateUtils.getCurrentTime().substring(0, 10));//创建日期
+		//将考试成绩存入考试主表和明细表
+		testmainService.saveExamResult(pd);
+		return pd;
 	}	
 }
